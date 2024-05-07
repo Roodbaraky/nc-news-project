48d3ddf445484606e318024b18c0ea25c04b05d0
Koo <kooroshr@hotmail.com>
2024-05-07 11:42:27 +0100

    added loader anim component and amended date format with moment.js

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './ArticleCard.css'
+import moment from 'moment'
 
 export const ArticleCard = ({ article, setArticle }) => {
 
@@ -13,7 +14,7 @@ export const ArticleCard = ({ article, setArticle }) => {
                 <img src={article.article_img_url} alt="" />
                 <p>{article.votes} votes</p>
                 <p>{article.comment_count} comments</p>
-                <p>{article.created_at}</p>
+                <p>{moment(article.created_at).format('DD/MM/YY, h:mm:ss a')}</p>
             </div>
         </Link>
     )
--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -1,24 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getArticles } from '../../../APIs'
 import { ArticleCard } from '../ArticleCard/ArticleCard'
 import './Articles.css'
+import { LoadingSpinner } from '../LoadingSpinner/LoadingSpinner'
+
 
 export const Articles = ({ articles, setArticles, setArticle }) => {
+    const [isLoading, setIsLoading] = useState(true)
+
     useEffect(() => {
         getArticles()
             .then((articles) => {
                 setArticles(articles)
-                
+                setIsLoading(false)
+
             })
     }, [articles])
 
     return (
         <div className='articles-container'>
-            {articles.map((article) => {
+            {isLoading
+            ? <LoadingSpinner/>
+            :articles.map((article) => {
                 return <ArticleCard
                     key={article.article_id}
                     article={article}
-                    
+
                 />
             })}
         </div>
