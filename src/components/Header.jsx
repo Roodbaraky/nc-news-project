d30734be89ed488eae93c8bed3c03b2528ba51a4
Koo <kooroshr@hotmail.com>
2024-05-19 23:53:34 +0100

    change to attempt to auto update netlify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,6 @@ export const Header = ({ Navbar }) => {
   return (
 
     <header className='  z-10 w-full flex-row place-content-center bg-bkg text-content rounded-lg   fixed shadow-2xl'>{Navbar}</header>
-
+//
   ) 
 }
