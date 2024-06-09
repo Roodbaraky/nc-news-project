2aed44fe3bf7bd23d75933115010e7c508d04473
Koo Roodbaraky <kooroshr@hotmail.com>
2024-06-09 12:39:50 +0100

    updated voting to use useQuery / useMutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Header } from "./components/Header"
+import { Header } from "./components/Header";
 import { Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home"
+import { Home } from "./pages/Home";
 import { Article } from "./pages/Article";
 import { useEffect, useState } from "react";
 import { ErrorContext, UserContext } from "./context/context";
@@ -10,33 +10,42 @@ import { User } from "./types/User";
 import { CustomError } from "./types/Error";
 
 
-
 function App() {
-  const [user, setUser] = useState<User | null>(null)
-  const [error, setError] = useState<CustomError | null>(null)
+  const [user, setUser] = useState<User | null>(null);
+  const [error, setError] = useState<CustomError | null>(null);
+
   useEffect(() => {
-    if (error) { (document.getElementById('errorModal') as HTMLDialogElement).showModal() }
-  })
+    if (error) {
+      (document.getElementById("errorModal") as HTMLDialogElement).showModal();
+    }
+  });
   return (
     <>
       <UserContext.Provider value={{ user, setUser }}>
         <ErrorContext.Provider value={{ error, setError }}>
-          <section id='app-container' className='flex flex-col h-full w-full align-items-center bg-bkg text-content'>
-            <Header />
-            <Routes>
-              <Route path='/' element={<Home />} >
-                <Route path='/:topic' element={<Home />} />
-              </Route>
-              <Route path='*' element={<ErrorPage error={error} />} />
-              <Route path='/article/:article_id' element={<Article />} />
-              <Route path='/error' element={<ErrorPage error={error} />} />
-            </Routes>
-            {error !== null && <ErrorPopUp error={error} setError={setError} />}
-          </section>
+       
+            <section
+              id="app-container"
+              className="flex flex-col h-full w-full align-items-center bg-bkg text-content"
+            >
+              <Header />
+              <Routes>
+                <Route path="/" element={<Home />}>
+                  <Route path="/:topic" element={<Home />} />
+                </Route>
+                <Route path="*" element={<ErrorPage error={error} />} />
+                <Route path="/article/:article_id" element={<Article />} />
+                <Route path="/error" element={<ErrorPage error={error} />} />
+              </Routes>
+              {error !== null && (
+                <ErrorPopUp error={error} setError={setError} />
+              )}
+            </section>
+        
         </ErrorContext.Provider>
       </UserContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
